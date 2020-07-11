88a3472017232414ef8ff11df6912e30b5d73ee7
Victor Johan <vicjohan23@gmail.com>
2020-07-11 00:29:49 -0400

    SE CREO LA CLASE EstudiantesBLL

---
+++ BLL/EstudiantesBLL.cs
@@ -0,0 +1,140 @@
+﻿using EstudiantePrestamoReportes.DAL;
+using EstudiantePrestamoReportes.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EstudiantePrestamoReportes.BLL
+{
+    public class EstudiantesBLL
+    {
+        public static bool Guardar(Estudiantes estudiante)
+        {
+            if (!Existe(estudiante.Matricula))
+                return Insertar(estudiante);
+            else
+                return Modificar(estudiante);
+        }
+
+        public static bool Existe(int matricula)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                ok = contexto.Estudiantes.Any(e => e.Matricula == matricula);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        private static bool Insertar(Estudiantes estudiante)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                contexto.Estudiantes.Add(estudiante);
+                ok = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        private static bool Modificar(Estudiantes estudiante)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                contexto.Database.ExecuteSqlRaw($"Delete FROM EstudianteDetalle Where Matricula={estudiante.Matricula}");
+                foreach (var anterior in estudiante.estudianteDetalles)
+                {
+                    contexto.Entry(anterior).State = EntityState.Added;
+                }
+                contexto.Entry(estudiante).State = EntityState.Modified;
+                ok = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        public static Estudiantes Buscar(int matricula)
+        {
+            Contexto contexto = new Contexto();
+            Estudiantes estudiante;
+
+            try
+            {
+                estudiante = contexto.Estudiantes.Find(matricula);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return estudiante;
+        }
+
+        public static bool Eliminar(int matricula)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                var encontrado = contexto.Estudiantes.Find(matricula);
+                contexto.Entry(encontrado).State = EntityState.Deleted;
+                ok = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+    }
+}
